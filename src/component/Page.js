50be026f05bab790cffb393c44661bc2ade5c531
Zhe Xu <lyxuzhe@gmail.com>
2019-02-07 01:35:24 -0500

    modify css style

--- src/component/Page.js
+++ src/component/Page.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import TweetList from './TweetList'
 import TweetPost from './TweetPost'
+import SideBar from './SideBar';
 
 class Page extends Component {
   state = {
@@ -46,8 +47,8 @@ class Page extends Component {
     return (
       <div className="container">
 
-        <div className="col-2of5 bg-white profile">
-          <div className="profile-content"></div>
+        <div className="col-2of5 bg-white">
+          <SideBar avatar={avatar}/>
         </div>
 
         <div className="col-3of5 bg-white">
