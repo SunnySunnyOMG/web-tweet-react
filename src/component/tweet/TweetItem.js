6d150aa2f498f810b6b59f7e4d12563111bdd432
Zhe Xu <lyxuzhe@gmail.com>
2019-02-20 21:42:39 -0500

    fix avatar

--- src/component/tweet/TweetItem.js
+++ src/component/tweet/TweetItem.js
@@ -3,6 +3,7 @@ import moment from 'moment';
 
 import { Route } from 'react-router-dom';
 import TweetDelete from './TweetDelete'
+import defaultAvatarUrl from '../../img/sample-avatar.png'
 
 function TweetItem(props) {
 
@@ -19,10 +20,11 @@ function TweetItem(props) {
         }
     } = props;
 
+    const _avatarUrl = avatarUrl.startsWith('/') ? defaultAvatarUrl : avatarUrl
     return (
         <div className="tweet">
             <div className="row relative">
-                <img className="tweet-avatar" src={avatarUrl} alt="avatar" />
+                <img className="tweet-avatar" src={_avatarUrl} alt="avatar" />
                 <h4><b>{name}</b></h4>
                 <h5>@{username}</h5>
                 <h5>{moment(createdAt).calendar()}</h5>
